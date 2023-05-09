efa369f6dfbb2c1903392190b979f9185f4b37eb
reddimasu <rsivaseshu@gmail.com>
2023-05-09 09:08:57 +0530

    ec2 instance creation

--- ec2/main.tf
+++ ec2/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 4.0"
     }
   }
+  backend "s3" {
+    bucket = "rssr-tf-state-file-bucket"
+    key    = "test-env/ec2"
+    region = "ap-south-1"
+    profile = "aws-dev-cred"
+  }
 }
 
 provider "aws" {
